e297a44a69a56542838a1fc659297504a4d367c1
Raghav Dalmia <vahgar.tisn@gmail.com>
2023-08-05 22:34:51 +0530

    Update run_server commands

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,8 @@
+sudo cd /home/ubuntu/project/ReviewManager
+source venv/bin/activate
+python manage.py collectstatic
+python manage.py makemigrations
+python manage.py migrate
 sudo systemctl daemon-reload
 sudo systemctl restart gunicorn
 sudo systemctl restart nginx
