af2204bf81d6513b476e00a24dc22c7d25b26f4e
Raghav Dalmia <vahgar.tisn@gmail.com>
2023-08-05 21:23:40 +0530

    Update logExceptionMiddleware with auth check

--- middleware/logExceptionMiddleware.py
+++ middleware/logExceptionMiddleware.py
@@ -14,11 +14,13 @@ class LogRequestExceptionMiddleware(MiddlewareMixin):
         if request.path in self.EXCLUDE_PATHS:
             return
         self.log_error("Path: " + request.path)
-        self.log_error("Username: " + request.user.username)
-        self.log_error("Email: " + request.user.email)
+        if request.user.is_authenticated:
+            self.log_error("Username: " + request.user.username)
+            self.log_error("Email: " + request.user.email)
         self.log_error("Timestamp: " + str(datetime.now()))
         self.log_error(request.body)
         self.log_error(traceback.format_exc())
+        self.log_error("----------------------\n\n")
         return render(request, './components/error.html')
     
     @staticmethod
